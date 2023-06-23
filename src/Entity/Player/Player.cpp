ab71ea2b7d585ae354894ca6b0d1b7150ad3537c
Tammerodev <lauri.etelamaki@gmail.com>
2023-06-23 19:57:22 +0300

    Created git

---
+++ src/Entity/Player/Player.cpp
@@ -0,0 +1,30 @@
+#include "Player.hpp"
+
+PlayerState* PlayerState::walkState = new WalkState();
+PlayerState* PlayerState::idleState = new IdleState();
+PlayerState* PlayerState::jumpState = new JumpState();
+
+PlayerState* PlayerState::currentState = PlayerState::idleState;
+
+void Player::load() {
+    if(!tx.loadFromFile("/media/lauri/acc1d3fc-a54d-465a-b6f6-116e7faa91c3/IntePLAs/res/img/Player/Player.png")) {perror("Could not load player texture");};
+    spr.setTexture(tx);
+
+}
+void Player::update(float dt) {
+    PlayerState::currentState->update(pos,y_acceleration,dt);
+    PlayerState::currentState->input(grounded);
+    y_acceleration += gravity * dt;
+
+    spr.setPosition(pos.x, pos.y);
+    pos.y += y_acceleration ;
+
+    update_hitboxtop();
+    update_hitboxbottom();
+    update_hitboxleft();
+    update_hitboxright();
+}
+
+void Player::draw(sf::RenderTarget & window) {
+    PlayerState::currentState->draw(window, spr);
+}
