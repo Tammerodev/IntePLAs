101d3731845cbe3ac3d9e404429b7a0bfa35dc4b
Lauri Etelamaki <lauri.etelamaki@gmail.com>
2024-04-26 22:03:33 +0300

    Changed cmakecache

--- build/CMakeFiles/3.25.1/CMakeSystem.cmake
+++ build/CMakeFiles/3.25.1/CMakeSystem.cmake
@@ -5,11 +5,11 @@ set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/lauri/Documents/Games/inteplas/Toolchain-mingw.cmake")
 
-set(CMAKE_SYSTEM "Linux-5.15.0-105-generic")
-set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_VERSION "5.15.0-105-generic")
-set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_SYSTEM "Windows")
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
 
-set(CMAKE_CROSSCOMPILING "FALSE")
+set(CMAKE_CROSSCOMPILING "TRUE")
 
 set(CMAKE_SYSTEM_LOADED 1)
