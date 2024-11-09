894685eb3d52a7cec136c63c7be12bffd11ca143
Lauri Etelamaki <lauri.etelamaki@gmail.com>
2024-11-09 12:50:23 +0200

    changing os

--- src/Elements/GravityFluid.hpp
+++ src/Elements/GravityFluid.hpp
@@ -10,16 +10,16 @@ class GravityFluid : public GravityElement {
         void run_rules(ChunkIndexer& world, sf::Vector2i& nextWaterPos) {
             int res = 0;
 
+            if(!math::randProp(101 - viscosity)) return;
+
             if(world.boundGetVoxelAt(nextWaterPos.x + 1, nextWaterPos.y).value == 0) {
                 res = 1;
             }
             if(world.boundGetVoxelAt(nextWaterPos.x - 1, nextWaterPos.y).value == 0) {
                 res = -1;
             }
-            if(world.boundGetVoxelAt(nextWaterPos.x - 1, nextWaterPos.y).value == 0 && world.boundGetVoxelAt(nextWaterPos.x + 1, nextWaterPos.y).value == 0) {
-                if(math::randProp(101 - viscosity)) {
-                    res = math::randIntInRange(-1, 1);
-                }
+            if(world.boundGetVoxelAt(nextWaterPos.x - 1, nextWaterPos.y).value == 0 && world.boundGetVoxelAt(nextWaterPos.x + 1, nextWaterPos.y).value == 0) {   
+                res = math::randIntInRange(-1, 1);           
             }
 
             nextWaterPos.x += res;
--- src/Elements/MoltenMetal.hpp
+++ src/Elements/MoltenMetal.hpp
@@ -6,13 +6,15 @@ class MoltenMetal : public GravityFluid {
         MoltenMetal(int xpos, int ypos, int type, int temp) : GravityFluid(xpos, ypos) {
             color = elm::getInfoFromType(type).color;
             value = type;
-            viscosity = 70;
+            viscosity = 10;
 
             temperature = temp;
         }
 
         void fluidUpdate(ChunkIndexer& world) {
             temperature--;
+            viscosity++;
+
             if(temperature < 100 && world.boundGetVoxelAt(x, y + 1).value != 0) {
                 if(world.boundGetVoxelAt(x, y + 1).value == 6 && timeOnAlum < 10) { // Al
                     timeOnAlum++;
